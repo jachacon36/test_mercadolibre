25e4ee6e4575fbd901a6a68f1ca4150e6c8ad965
Armando Chacon <jarmando.chacon36@gmail.com>
2025-03-17 11:57:50 -0500

    refactor: reorganize project folder structure
    - Moved `MainActivity.kt` to `activity` package
    - Moved `MLApplication.kt` to `application` package
    - Updated package declarations in moved files
    - Ensured all imports are correctly updated

---
+++ app/src/main/java/com/example/testmercadolibre/application/MLApplication.kt
@@ -0,0 +1,8 @@
+package com.example.testmercadolibre.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MLApplication: Application() {
+}
