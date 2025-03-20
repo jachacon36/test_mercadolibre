c5ed7e510810b551a85a5bcdf54130665e28b45a
Armando Chacon <jarmando.chacon36@gmail.com>
2025-03-20 11:45:54 -0500

    feat: enhance ActionButtons component with improved styling and preview

--- app/src/main/java/com/example/testmercadolibre/presentation/presentation/composables/ActionsButtons.kt
+++ app/src/main/java/com/example/testmercadolibre/presentation/presentation/composables/ActionsButtons.kt
@@ -3,37 +3,49 @@ package com.example.testmercadolibre.presentation.presentation.composables
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.testmercadolibre.R
+import com.example.testmercadolibre.ui.theme.Blue
+import com.example.testmercadolibre.ui.theme.BuyButton
+import com.example.testmercadolibre.ui.theme.FavoriteButton
 
 @Composable
 fun ActionButtons() {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(15.dp)
     ) {
         Button(
             onClick = { /*TODO*/ },
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 10.dp)
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(BuyButton),
+            shape = RoundedCornerShape(6.dp)
         ) {
             Text(text = stringResource(id = R.string.buy))
         }
         Button(
             onClick = { /*TODO*/ },
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 20.dp)
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(FavoriteButton),
+            shape = RoundedCornerShape(6.dp)
         ) {
-            Text(text = stringResource(id = R.string.add_car), color = Color.Blue)
+            Text(text = stringResource(id = R.string.add_car), color = Blue)
         }
     }
+}
+
+@Preview
+@Composable
+fun ActionButtonsPreview() {
+    ActionButtons()
 }
